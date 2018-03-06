b046e09d21de41d007f6a8b7671f71589f6ee079
NiallRees <niallrwoodward@gmail.com>
2018-03-06 17:51:02 +0000

    Added centreMap function which positions the camera around both user and bike coordinates

--- app/src/main/java/com/example/rahulberry/googlemaps/MapFragment.java
+++ app/src/main/java/com/example/rahulberry/googlemaps/MapFragment.java
@@ -275,6 +275,27 @@ public class MapFragment extends SupportMapFragment
         }
     }
 
+    public void centreMap() {
+        double lon1 = bike.longitude;
+        double lat1 = bike.latitude;
+        double lon2 = user.longitude;
+        double lat2 = user.latitude;
+
+        double dLon = Math.toRadians(lon2 - lon1);
+
+        lat1 = Math.toRadians(lat1);
+        lat2 = Math.toRadians(lat2);
+        lon1 = Math.toRadians(lon1);
+
+        double Bx = Math.cos(lat2) * Math.cos(dLon);
+        double By = Math.cos(lat2) * Math.sin(dLon);
+        double lat3 = Math.atan2(Math.sin(lat1) + Math.sin(lat2), Math.sqrt((Math.cos(lat1) + Bx) * (Math.cos(lat1) + Bx) + By * By));
+        double lon3 = lon1 + Math.atan2(By, Math.cos(lat1) + Bx);
+
+        LatLng latLng = new LatLng(lat3, lon3);
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,16));
+    }
+
     @Override
     public void onConnectionSuspended(int i) {}
 
